6367a1edd0b0f58ccf52141a3756934ae0a73c0b
Root <Root>
2017-05-05 21:29:57 -0300

    pequenas correções para melhor visibilidade.

--- chatbot/start_bot.py
+++ chatbot/start_bot.py
@@ -1,10 +1,16 @@
 import aiml
+import os
 
 # Create the kernel and learn AIML files
 kernel = aiml.Kernel()
 kernel.learn("std-standard.xml")
 kernel.respond("load aiml b")
 
+# clean the prompt
+os.system('CLS')
+
 # Press CTRL-C to break this loop
 while True:
-    print kernel.respond(raw_input("Enter your message >> "))
+    #print kernel.respond(raw_input("Enter your message >> "))
+    message = raw_input("Enter your message >> ")
+    print "Alice >> " + kernel.respond(message)
